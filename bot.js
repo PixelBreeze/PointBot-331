2471e298b32ec02737efe9590c4dbd96e8528d24
Zaro38 <github@niek.goudswaard.nl>
2015-04-10 14:27:17 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,11 +1,15 @@
-var version = '0.0.1';
+var version = '0.0.2';
+//
+//Version 0.0.2
+//Disabled the warning, its not really useful
+//Changed the \ in the command to .
 //
 //Version 0.0.1
 //Created first version of PointBot-331
 //
 
 API.sendChat('PointBot-331 v' + version + ' is online! Type \pb help for a list of commands.');
-API.sendChat('Warning: this bot is still under development; points you get are not final and it doesn\'t have to work 100% correct.')
+//API.sendChat('Warning: this bot is still under development; points you get are not final and it doesn\'t have to work 100% correct.')
 
 //This function runs when someone says something
 function all(data) {
@@ -13,7 +17,7 @@ function all(data) {
 	var username = data.un;
 	var userid = data.uid;
 	var messageid = data.cid;
-	if (message.slice(0,3) != '\pb') {
+	if (message.slice(0,3) != '.pb') {
 		return;
 	}
 	if (message.split(' ').length < 2) {
@@ -22,7 +26,7 @@ function all(data) {
 	
 	//Check what the message is, and if it's an existing PointBot command, respond
 	switch (message) {
-		case '\pb help':
+		case '.pb help':
 			API.sendChat('[PB] [@' + username + '] Click here for a list of commands: https://github.com/DragonCzz/PointBot-331/blob/master/commands.md');
 		break;
 	}
