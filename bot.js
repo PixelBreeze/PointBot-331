ca5bb1aeeab88fadafe0ba8ed63ef432634d0c7d
Zaro38 <github@niek.goudswaard.nl>
2015-04-12 19:59:43 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,8 +11,19 @@ var version = '0.0.2';
 //Created first version of PointBot-331
 //
 
-API.sendChat('PointBot-331 v' + version + ' is online! Type \pb help for a list of commands.');
+API.sendChat('PointBot-331 v' + version + ' is online! Type .pb help for a list of commands.');
 //API.sendChat('Warning: this bot is still under development; points you get are not final and it doesn\'t have to work 100% correct.')
+
+var users = {
+	'5792994' : {
+		'points' : 0,
+		'username' : 'Zaro38'
+	},
+	'5010460' : {
+		'points' : 0,
+		'username' : 'Nuvm'
+	}
+}
 
 //This function runs when someone says something
 function all(data) {
@@ -29,17 +40,51 @@ function all(data) {
 	}
 	
 	//Check what the message is, and if it's an existing PointBot command, respond
-	switch (message) {
-		case '.pb help':
+	switch (message.split(' ')[1]) {
+		case 'help':
 			API.sendChat('[PB] [@' + username + '] Click here for a list of commands: https://github.com/DragonCzz/PointBot-331/blob/master/commands.md');
 			break;
-		case '.pb stop':
+		case 'stop':
 			if (userrole > 2) {
 				API.sendChat('[PB] [@' + username + '] Stopping PointBot-331!');
 				API.off(API.CHAT,all);
 			}
 			break;
+		case 'points':
+			//if the command is ran by bouncer or higher and someone is @mentioned in it
+			if (userrole > 1 && message.split('@').length > 1) {
+				//respond
+				var userTargetName = message.slice(message.indexOf('@') + 1,255);
+				API.sendChat('[PB] [@' + username + '] The user @' + userTargetName + ' has ' + checkPoints(userTargetName) + ' points.')
+			}
+			else {
+				//respond
+				API.sendChat('[PB] [@' + username + '] You have ' + checkPoints(username) + ' points.')	
+			}
+			break;
+		case 'give':
+			break;
 	}
+}
+
+function checkPoints(username) {
+	var userid;
+	var i = 0;
+	//get the ID of the target user and assign it to userTargetId
+	while (i < API.getUsers().length) {
+		if(API.getUsers()[i].username == username) {
+			userid = API.getUsers()[i].id;
+			i = API.getUsers().length;
+		}
+		i++;
+	}
+	//if the person specified is not yet in the user list
+	if (users[userid] == undefined) {
+		users[userid] = {'points':0,'username':username};
+	}
+	//get the points of the user
+	var targetUserPoints = users[userid].points;
+	return targetUserPoints;
 }
 
 //Let the function "all" run when there is a new message
